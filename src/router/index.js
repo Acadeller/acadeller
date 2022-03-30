5fe2e5367172876dfe0775fbf3d998040fb93018
César Álvarez <cesaralv97@gmail.com>
2022-03-30 23:37:07 +0200

    Create routes for register views

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ import LessonsView from "../views/LessonsView.vue";
 import TagsView from "../views/TagsView.vue";
 import AboutView from "../views/AboutView.vue";
 import ErrorView from "../views/ErrorView.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import ForgotPasswordView from "../views/ForgotPasswordView.vue";
 
 Vue.use(VueRouter);
 
@@ -48,6 +51,30 @@ const routes = [
     component: AboutView,
     meta: {
       title: "Acerca De"
+    }
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+    meta: {
+      title: "Login"
+    }
+  },
+  {
+    path: "/registro",
+    name: "register",
+    component: RegisterView,
+    meta: {
+      title: "Registro"
+    }
+  },
+  {
+    path: "/reset-contraseña",
+    name: "ForgotPassword",
+    component: ForgotPasswordView,
+    meta: {
+      title: "Reset de Contraseña"
     }
   },
   {
